82acdde0293261043ea8efcd9d2debf9bb1570e5
Fabrício Ronchi <fabricioronchii@gmail.com>
2015-06-28 11:38:18 -0300

    HUE

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,22 +10,22 @@ module.exports = function(grunt) {
     uncss: {
       dist: {
         files: {
-          'public/dist/css/index.css': ['views/css.html', 'views/index.html', 'public/tpl/home.html', 'public/tpl/maps.html']
+          'public/dist/css/login.css': ['views/login.html']
         }
       }
     },
     cssmin: {
       target: {
         files: {
-          'public/dist/css/index.min.css': ['public/dist/css/index.css']
+          'public/dist/css/login.min.css': ['public/dist/css/login.css']
         }
       }
     },
     uglify: {
       my_target: {
         files: {
-          'public/dist/js/login.min.js': ['public/vendor/angular/angular.js',
-          'public/vendor/angularstorage/ngStorage.min.js', 'public/js/login.js']
+          //'public/dist/js/login.min.js': ['public/vendor/angular/angular.js',
+          //'public/vendor/angularstorage/ngStorage.min.js', 'public/js/login.js']
         }
       }
     }
