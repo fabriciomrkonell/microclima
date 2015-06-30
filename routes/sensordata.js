fe2df9014a6cba7523fba6f36c848579f0b51bc5
Fabrício Ronchi <fabricioronchii@gmail.com>
2015-06-30 19:21:38 -0300

    Ultimas

--- routes/sensordata.js
+++ routes/sensordata.js
@@ -5,15 +5,24 @@ var models  = require('../models'),
     error = require('../error/error'),
     router  = express.Router();
 
+router.get('/', function(req, res) {
+ 	models.SensorData.findAll({
+    attributes: ['SensorId', 'StationId', 'valueData'],
+  }).then(function(data) {
+    res.send({ error: 0, data: data });
+  }).catch(function(err) {
+    error.sendError(res, err);
+  });
+});
+
 router.post('/', function(req, res) {
-	console.log(req.body);
   if(req.body.stationId){
   	if(req.body.data.length > 0){
   		var values = [];
   		for(var i = 0; i < req.body.data.length; i++){
   			values.push({ StationId: req.body.stationId, SensorId: req.body.data[i].sensorId, valueData: req.body.data[i].valueData });
   		}
-  		models.Sensor.bulkCreate(values).then(function(data) {
+  		models.SensorData.bulkCreate(values).then(function(data) {
 	      res.send({ error: 0 });
 	    }).catch(function(err) {
 	      res.send({ error: 1 });
