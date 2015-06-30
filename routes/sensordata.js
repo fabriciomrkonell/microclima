2366d8eae19ac62b14703e10a1e1de175cebcbd8
Fabrício Ronchi <fabricioronchii@gmail.com>
2015-06-30 19:39:19 -0300

    Ultimas

--- routes/sensordata.js
+++ routes/sensordata.js
@@ -17,18 +17,22 @@ router.get('/', function(req, res) {
 
 router.post('/', function(req, res) {
   if(req.body.stationId){
-  	if(req.body.data.length > 0){
-  		var values = [];
-  		for(var i = 0; i < req.body.data.length; i++){
-  			values.push({ StationId: req.body.stationId, SensorId: req.body.data[i].sensorId, valueData: req.body.data[i].valueData });
-  		}
-  		models.SensorData.bulkCreate(values).then(function(data) {
-	      res.send({ error: 0 });
-	    }).catch(function(err) {
-	      res.send({ error: 1 });
-	    });
+  	if(req.body.data){
+  		if(req.body.data.length > 0){
+	  		var values = [];
+	  		for(var i = 0; i < req.body.data.length; i++){
+	  			values.push({ StationId: req.body.stationId, SensorId: req.body.data[i].sensorId, valueData: req.body.data[i].valueData });
+	  		}
+	  		models.SensorData.bulkCreate(values).then(function(data) {
+		      res.send({ error: 0 });
+		    }).catch(function(err) {
+		      res.send({ error: 1, message: 'Os valores estão inválidos!' });
+		    });
+	  	}else{
+	  		res.send({ error: 1, message: 'Estação sem valores!' });
+	  	}
   	}else{
-  		res.send({ error: 1 });
+  		res.send({ error: 1, message: 'Valores não encontrados!' });
   	}
   }else{
   	res.send({ error: 1, message: 'Estação não encontrada!' });
