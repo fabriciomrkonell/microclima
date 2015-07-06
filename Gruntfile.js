1e055c4dbe50d311916519fb20d2e7286b14cbe0
Fabrício Ronchi <fabricioronchii@gmail.com>
2015-07-06 16:27:12 -0300

    Enter

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
   grunt.initConfig({
-    uncss: {
+    /*uncss: {
       dist: {
         files: {
           'public/dist/css/login.css': ['views/login.html']
@@ -20,17 +20,17 @@ module.exports = function(grunt) {
           'public/dist/css/login.min.css': ['public/dist/css/login.css']
         }
       }
-    },
+    },*/
     uglify: {
       my_target: {
         files: {
-          //'public/dist/js/login.min.js': ['public/vendor/angular/angular.js',
-          //'public/vendor/angularstorage/ngStorage.min.js', 'public/js/login.js']
+          'public/dist/js/login.min.js': ['public/vendor/angular/angular.js',
+          'public/vendor/angularstorage/ngStorage.min.js', 'public/js/login.js']
         }
       }
     }
   });
 
-  grunt.registerTask('default', ['uncss', 'cssmin', 'uglify']);
+  grunt.registerTask('default', ['uglify']);
 
 };
--- public/js/login.js
+++ public/js/login.js
@@ -13,18 +13,20 @@ angular.module('login-app').controller('login-ctrl', ['$scope', '$http', 'store'
 		error: false
 	});
 
-	$scope.login = function(account, remember){
-		saveStore(account, remember);
-		$scope.error = false;
-		$http.post('/login', account).success(function(data){
-			if(data.error){
+	$scope.login = function(account, remember, e){
+		if(e.keyCode == 13 || e.type == 'click'){
+			$scope.error = false;
+			$http.post('/login', account).success(function(data){
+				if(data.error){
+					$scope.error = true;
+				}else{
+					saveStore(account, remember);
+					window.location = '/';
+				}
+			}).error(function(error){
 				$scope.error = true;
-			}else{
-				window.location = '/';
-			}
-		}).error(function(error){
-			$scope.error = true;
-		});
+			});
+		}
 	};
 
 	function saveStore(account, remember){
