b9ad65a449e73492fd2bd6c0bc0c18d9700993c5
Fabrício Ronchi <fabricioronchii@gmail.com>
2015-07-21 15:50:38 -0300

    Timezone

--- models/index.js
+++ models/index.js
@@ -5,7 +5,9 @@ var fs = require("fs"),
 		Sequelize = require("sequelize"),
 		env = process.env.NODE_ENV || "development",
 		config = require(__dirname + '/../config/config.json')[env],
-		sequelize = new Sequelize(config.database, config.username, config.password, config),
+		sequelize = new Sequelize(config.database, config.username, config.password, {
+			timezone: '-03:00'
+		}),
 		db = {};
 
 fs.readdirSync(__dirname).filter(function(file) {
