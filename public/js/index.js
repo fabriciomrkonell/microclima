75e0cd0338a3d38b8af45a3fea44ce3a3bd1df15
Fabrício Ronchi <fabricioronchii@gmail.com>
2015-08-01 04:16:43 -0300

    Casa decimal

--- public/js/index.js
+++ public/js/index.js
@@ -29,24 +29,27 @@ define(['angularAMD', 'ngRoute', 'ngAnimate'], function (angularAMD, ngRoute, ng
 
   	this.showChart = function(type, data, label){
 
+  		data = [{"valueData":"22.8","dateCreate":"2015-07-31 00:02"},{"valueData":"22.8","dateCreate":"2015-07-31 00:11"},{"valueData":"22.8","dateCreate":"2015-07-31 00:19"},{"valueData":"22.7","dateCreate":"2015-07-31 00:27"},{"valueData":"22.7","dateCreate":"2015-07-31 00:36"},{"valueData":"22.7","dateCreate":"2015-07-31 00:44"},{"valueData":"22.7","dateCreate":"2015-07-31 00:52"},{"valueData":"22.7","dateCreate":"2015-07-31 01:01"},{"valueData":"22.6","dateCreate":"2015-07-31 01:09"},{"valueData":"22.6","dateCreate":"2015-07-31 01:17"},{"valueData":"22.6","dateCreate":"2015-07-31 01:26"},{"valueData":"22.6","dateCreate":"2015-07-31 01:34"},{"valueData":"22.6","dateCreate":"2015-07-31 01:42"},{"valueData":"22.6","dateCreate":"2015-07-31 01:51"},{"valueData":"22.5","dateCreate":"2015-07-31 01:59"},{"valueData":"22.5","dateCreate":"2015-07-31 02:07"},{"valueData":"22.5","dateCreate":"2015-07-31 02:16"},{"valueData":"22.4","dateCreate":"2015-07-31 02:24"},{"valueData":"22.4","dateCreate":"2015-07-31 02:32"},{"valueData":"22.4","dateCreate":"2015-07-31 02:41"},{"valueData":"22.4","dateCreate":"2015-07-31 02:49"},{"valueData":"22.4","dateCreate":"2015-07-31 02:57"},{"valueData":"22.4","dateCreate":"2015-07-31 03:06"},{"valueData":"22.3","dateCreate":"2015-07-31 03:14"},{"valueData":"22.3","dateCreate":"2015-07-31 03:22"},{"valueData":"22.2","dateCreate":"2015-07-31 03:31"},{"valueData":"22.2","dateCreate":"2015-07-31 03:39"},{"valueData":"22.2","dateCreate":"2015-07-31 03:47"},{"valueData":"22.2","dateCreate":"2015-07-31 03:56"},{"valueData":"22.2","dateCreate":"2015-07-31 04:04"},{"valueData":"22.1","dateCreate":"2015-07-31 04:12"},{"valueData":"22.1","dateCreate":"2015-07-31 04:21"},{"valueData":"22.1","dateCreate":"2015-07-31 04:29"},{"valueData":"22.1","dateCreate":"2015-07-31 04:38"},{"valueData":"22.1","dateCreate":"2015-07-31 04:46"},{"valueData":"22","dateCreate":"2015-07-31 04:54"},{"valueData":"22","dateCreate":"2015-07-31 05:03"},{"valueData":"22","dateCreate":"2015-07-31 05:11"},{"valueData":"22","dateCreate":"2015-07-31 05:19"},{"valueData":"22","dateCreate":"2015-07-31 05:28"},{"valueData":"22","dateCreate":"2015-07-31 05:36"},{"valueData":"21.9","dateCreate":"2015-07-31 05:44"},{"valueData":"21.9","dateCreate":"2015-07-31 05:53"},{"valueData":"21.9","dateCreate":"2015-07-31 06:01"},{"valueData":"21.8","dateCreate":"2015-07-31 06:09"},{"valueData":"21.8","dateCreate":"2015-07-31 06:18"}];
+
   		var ymax = data;
   		if(ymax.length > 0){
-  			ymax = parseFloat(parseFloat(ymax.sort(propComparator('valueData')).reverse()[0].valueData).toFixed(2));
+  			ymax = parseFloat(ymax.sort(propComparator('valueData')).reverse()[0].valueData);
   		}else{
   			ymax = 0;
   		}
 
   		var ymin = data;
   		if(ymin.length > 0){
-  			ymin = parseFloat(parseFloat(ymin.sort(propComparator('valueData'))[0].valueData).toFixed(2));
+  			ymin = parseFloat(ymin.sort(propComparator('valueData'))[0].valueData);
   		}else{
   			ymin = 0;
   		}
 
-  		var interval = (ymax - ymin) * 0.1;
 
-  		ymax = ymax + interval;
-  		ymin = ymin - interval;
+  		var interval = (ymax - ymin) * 0.1;
+  		ymax = parseFloat((ymax + interval).toFixed(2));
+  		ymin = parseFloat((ymin - interval).toFixed(2));
+
 
   		var configs = {
         element: 'charts',
